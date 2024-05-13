6953a754b02b01d4553124ce11e289d444b54bac
Ezequiel Rindello <rindelloezequiel@gmail.com>
2024-05-13 17:44:01 -0300

    password hide/show

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,17 @@
+// password hide/show funtion
+document.addEventListener("DOMContentLoaded", function () {
+  const passwordBtn = document.getElementById("password-btn");
 
+  passwordBtn.addEventListener("click", () => {
+    const passwordInput = document.getElementById("pword");
+    const type = passwordInput.getAttribute("type");
+
+    if (type == "password") {
+      passwordInput.setAttribute("type", "text");
+      
+    } else {
+      passwordInput.setAttribute("type", "password");
+      
+    }
+  });
+});
