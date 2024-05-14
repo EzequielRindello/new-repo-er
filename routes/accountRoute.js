54a856b8d4b9d992c856fbf61ac4d4b4db0dafdf
Ezequiel Rindello <rindelloezequiel@gmail.com>
2024-05-14 11:27:46 -0300

    Data Validation Server side

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,12 +3,19 @@ const express = require("express");
 const router = new express.Router();
 const accController = require("../controllers/accountController");
 const utilities = require("../utilities");
+const regValidate = require("../utilities/account-validation");
 
 // Route to build inventory by classification view
 router.get("/login", utilities.handleErrors(accController.buildLogin));
 
 router.get("/register", utilities.handleErrors(accController.buildRegister));
 
-router.post('/register', utilities.handleErrors(accController.registerAccount))
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accController.registerAccount)
+);
 
 module.exports = router;
